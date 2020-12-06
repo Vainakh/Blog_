4543f3e1bf67af64352cad739ae6fb3f919db5e9
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-12-06 12:28:32 -0800

    Loading routes lazily

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
 import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 // import FullPost from './FullPost/FullPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
     render () {
         return (
@@ -40,10 +45,11 @@ class Blog extends Component {
                 <Route path="/new-post" render={() => <h1>New Post</h1>}/> */}
                 
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost}/> : null} 
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null} 
                     <Route path="/posts/" component={Posts}/>
+                    <Route render={() => <h1>You need to authenticate to access this route!</h1>}/>
                     {/* <Route path="/" component={Posts}/> */}
-                    <Redirect from='/' to='/posts/'/>
+                    {/* <Redirect from='/' to='/posts/'/> */}
                 </Switch>
             </div>
         );
